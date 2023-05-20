72f943a6bae6e9183a289aa1cb59efd54c394eb1
ohy2525 <dhgodbs2@naver.com>
2023-05-20 11:03:15 +0900

    영속성 컨텍스트2

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,27 +16,9 @@ public class JpaMain {
 
         try {
 
-            Member findMember = em.find(Member.class, 1L);
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//
-//            em.persist(member); // 멤버 저장
-            //findMember.setName("helloJPA"); // 멤버 수정
+            Member findMember1 = em.find(Member.class, 1L); // DB 에서 조회
+            Member findMember2 = em.find(Member.class, 1L); // 1차 개시에서 조회
 
-            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)   // JPQL 페이징 할 때 편리
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }*/
-            // 비영속
-            Member member = new Member();
-            member.setId(3L);
-            member.setName("HelloB");
-
-            // 영속 상태
-            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
