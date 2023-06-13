42536aad59424d29a9cf6ad75dfceeb9c4be8abc
ohy2525 <dhgodbs2@naver.com>
2023-06-13 23:51:22 +0900

    값 타입과 불변 객체

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -15,14 +15,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-
+            Address address = new Address("city", "ee", "ee");
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "ee", "ee"));
-            member.setWorkPeriod(new Period());
+            member.setUsername("member1");
+            member.setHomeAddress(address);
 
             em.persist(member);
-            
+
+            Member member2 = new Member();
+            member.setUsername("member2");
+            member.setHomeAddress(address);
+
+            em.persist(member2);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
