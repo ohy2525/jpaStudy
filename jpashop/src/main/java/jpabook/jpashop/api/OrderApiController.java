9614595bf2fa84debdb92e4c92ffae7a087afdad
ohy2525 <dhgodbs2@naver.com>
2023-08-06 11:36:41 +0900

    주문 조회 V3: 엔티티를 DTO로 변환-페치 조인 최적화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -49,7 +49,11 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
-        return orders;
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
     }
 
     @Getter
