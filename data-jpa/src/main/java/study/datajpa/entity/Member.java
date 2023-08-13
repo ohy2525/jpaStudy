cf0754a26559289546ed3905afcd8682f86cf493
ohy2525 <dhgodbs2@naver.com>
2023-08-13 21:55:23 +0900

    예제 도메인 모델

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -3,17 +3,21 @@ package study.datajpa.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     protected Member() {
     }
---
+++ data-jpa/src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,17 @@
+package study.datajpa.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany
+    private List<Member> members = new ArrayList<>();
+}
