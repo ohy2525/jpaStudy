b919e6cc29959d74a9e9fc37d5d33e23fa769381
ohy2525 <dhgodbs2@naver.com>
2023-08-17 14:40:28 +0900

    스프링 데이터 JPA 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,4 +35,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findListByUsername(String username);
     Member findMemberByUsername(String username);
     Optional<Member> findOptionalByUsername(String username);
+
+    @Query(value = "select m from Member m left join m.team t",
+            countQuery = "select count(m) from Member m") //복잡해지면 countQuery 분리
+    Page<Member> findByAge(int age, Pageable pageable);
 }
