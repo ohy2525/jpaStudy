b959a1f587e04a59f196a243e1685ce2e0244d69
ohy2525 <dhgodbs2@naver.com>
2023-08-17 16:43:10 +0900

    사용자 정의 Repository

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -253,4 +253,10 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+
+    @Test
+    public void callCustom() {
+        List<Member> members = memberRepository.findMemberCustom();
+    }
 }
